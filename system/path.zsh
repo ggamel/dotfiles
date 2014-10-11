167bb42c155964e329de3329b7b9306136fea318
Greg Gamel <ggamel@gmail.com>
2014-10-11 11:32:17 -0500

    Adjust system config stuff.

--- system/path.zsh
+++ system/path.zsh
@@ -2,3 +2,11 @@ export PATH="$HOME/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/u
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man"
 export NODE_PATH="/usr/local/lib/node"
+export NODE_PATH="$NODE_PATH:/usr/local/lib/node_modules"
+
+# Forecast.io ENV variable for 'weatherme' cmd
+export KEY=634a5adb0ded9cf61ea6bb9d2ae099b6
+export LATLON=33.5618,-101.9397
+
+export QT_DIR="$HOME/Qt/5.3/clang_64"
+export PATH="$PATH:$QT_DIR/bin"

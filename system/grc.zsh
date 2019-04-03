4f958b94f6b30fbede8f8dd296ac344cdb959d24
Greg Gamel <ggamel@gmail.com>
2019-04-03 16:54:18 -0500

    Add grc for ls

---
+++ system/grc.zsh
@@ -0,0 +1,19 @@
+# Check for brew and grc
+if [[ -f `command -v brew` ]]; then
+  grc_conf="`brew --prefix grc`/etc/grc.bashrc"
+  if [[ -f $grc_conf ]]; then
+    source $grc_conf
+    alias curl='colourify curl -s'
+  fi
+fi
+
+# grc overides for ls
+#   Made possible through contributions from generous benefactors like
+#   `brew install coreutils`
+if $(gls &>/dev/null)
+then
+  alias l="gls -lAhF --color=auto --group-directories-first"
+  alias ls="gls -F --color=auto"
+  alias ll="gls -lF --color=auto"
+  alias la="gls -AF --color=auto"
+fi

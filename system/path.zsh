a324fef0461c43dae0bc64fb8c33a99d1a1eb587
Greg Gamel <ggamel@gmail.com>
2019-04-03 16:25:00 -0500

    Clean path

--- system/path.zsh
+++ system/path.zsh
@@ -1,12 +1,18 @@
-export PATH="$HOME/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/npm:/usr/local/share/npm/bin"
+export PATH="$HOME/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/npm"
 
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man"
-export NODE_PATH="/usr/local/lib/node"
-export NODE_PATH="$NODE_PATH:/usr/local/lib/node_modules"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/usr/share/man:/usr/local/share/man"
+# export NODE_PATH="/usr/local/bin/node:/usr/local/bin/node_modules"
+# export NODE_PATH="$NODE_PATH:/usr/local/bin/node_modules"
 
 # Forecast.io ENV variable for 'weatherme' cmd
 export KEY=634a5adb0ded9cf61ea6bb9d2ae099b6
 export LATLON=33.5618,-101.9397
 
-export QT_DIR="$HOME/Qt/5.3/clang_64"
-export PATH="$PATH:$QT_DIR/bin"
+# export QT_DIR="$HOME/Qt/5.3/clang_64"
+# export PATH="$PATH:$QT_DIR/bin"
+
+# Android stuff for React Native
+export ANDROID_SDK="$HOME/Library/Android/sdk"
+export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
+# PATH="$HOME/Library/Android/sdk/tools:$HOME/Library/Android/sdk/platform-tools:${PATH}"
+export PATH
